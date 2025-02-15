b114d1280aeafd13baaa15aeaccfa3bc45a29ce8
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-02-15 23:44:33 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Nav from './nav/nav';
+import Hero from './hero/hero'
+import MainSection from './mainSection/mainSection'
+import Cards from './cards/cards'
+import Footer from './footer/Footer';
+
+
+function App() {
+
+
+  return (
+    <>
+    <Nav />
+    <Hero/>
+    <MainSection></MainSection>
+    <Cards></Cards>
+     <Footer />
+  </>
+  )
+}
+
+export default App
